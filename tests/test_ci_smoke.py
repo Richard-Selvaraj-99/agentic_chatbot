576a15bbda0cb20b36b96654af409c8b171250af
richard selvaraj <r10053506@gmail.com>
2026-03-01 18:03:08 +0530

    local test ran successfully using pytest

---
+++ tests/test_ci_smoke.py
@@ -0,0 +1,33 @@
+import os
+
+
+def test_import_dependencies():
+    """
+    Smoke test to ensure all critical dependencies install correctly.
+    """
+    import langchain
+    import langgraph
+    import langchain_community
+    import langchain_core
+    import langchain_groq
+    import langchain_openai
+    import faiss
+    import streamlit
+    import tavily
+
+"""""
+def test_app_import():
+   
+    import app
+
+
+def test_required_env_vars_exist():
+   
+    required_vars = [
+        "DOCKER_PASSWORD",
+        "DOCKER_USERNAME",
+        "HF_TOKEN"
+    ]
+
+    for var in required_vars:
+        assert os.getenv(var) is not None, f"{var} is missing" """
